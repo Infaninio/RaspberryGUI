28eeed38dc4e0d0c1645ef132d09fd1f705701da
Infaninio <infaninio@gmail.com>
2019-08-25 22:30:50 +0200

    MainWindow erstellt. Struktur wird noch einmal geändert werden.

--- src/mainWindow.py
+++ src/mainWindow.py
@@ -2,8 +2,9 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
+IMAGEPATH = "C:/Users/marti/source/repos/RaspberryGUI/img"
 
-class MainWindow(QMainWindow):
+class GenWindow(QMainWindow):
     """description of class"""
    
    
@@ -15,8 +16,10 @@ class MainWindow(QMainWindow):
         self.setGeometry(0,0,800,480)
         self.setWindowTitle('RaspberryTouch')
 
-        self.setObjectName("MainWindow")
-        self.setStyleSheet("QMainWindow#MainWindow{background-image: url(C://sync/rapibackresize.jpg);}")
+        self.setObjectName("GenWindow")
+
+        styleSheetStr = "QMainWindow#GenWindow{background-image: url(" + IMAGEPATH + "/rapibackresize.jpg" + ");}"
+        self.setStyleSheet(styleSheetStr)
 
 
         #Leiste
@@ -49,5 +52,59 @@ class MainWindow(QMainWindow):
         else:
             event.ignore()
 
+
+
+class MainWindow(GenWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        #String für den Button Hintergrund
+        styleSheetStr = "QPushButton{background-image: url(" + IMAGEPATH + "/icoback2kl.png" + ");}"
+
+        #Computer Button
+        computerBt = QPushButton(self)
+        computerBt.setGeometry(110,60,128,128)
+        computerBt.setFlat(True)
+        computerBt.setStyleSheet(styleSheetStr)
+        computerBt.setIcon(QIcon(IMAGEPATH + "/computer.png"))
+        computerBt.setIconSize(QSize(110,110))
+
+        #CD Button
+        cdBt = QPushButton(self)
+        cdBt.setGeometry(336,60,128,128)
+        cdBt.setFlat(True)
+        cdBt.setStyleSheet(styleSheetStr)
+        cdBt.setIcon(QIcon(IMAGEPATH + "/cd.png"))
+        cdBt.setIconSize(QSize(110,110))
+
+        #Radio Button
+        musikBt = QPushButton(self)
+        musikBt.setGeometry(562,60,128,128)
+        musikBt.setFlat(True)
+        musikBt.setStyleSheet(styleSheetStr)
+        musikBt.setIcon(QIcon(IMAGEPATH + "/musik.png"))
+        musikBt.setIconSize(QSize(110,110))
+
+        avBt = QPushButton(self)
+        avBt.setGeometry(110,232,128,128)
+        avBt.setFlat(True)
+        avBt.setStyleSheet(styleSheetStr)
+        avBt.setIcon(QIcon(IMAGEPATH + "/av.png"))
+        avBt.setIconSize(QSize(110,110))
+
+        beamerBt = QPushButton(self)
+        beamerBt.setGeometry(336,232,128,128)
+        beamerBt.setFlat(True)
+        beamerBt.setStyleSheet(styleSheetStr)
+        beamerBt.setIcon(QIcon(IMAGEPATH + "/beamer.png"))
+        beamerBt.setIconSize(QSize(110,110))
+
+        einstellungenBt = QPushButton(self)
+        einstellungenBt.setGeometry(562,232,128,128)
+        einstellungenBt.setFlat(True)
+        einstellungenBt.setStyleSheet(styleSheetStr)
+        einstellungenBt.setIcon(QIcon(IMAGEPATH + "/einstellungen.png"))
+        einstellungenBt.setIconSize(QSize(110,110))
 
 
